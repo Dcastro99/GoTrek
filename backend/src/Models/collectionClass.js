6ec284a00a5b095024b75a61f2a4d6b2a6eb8d3a
Danny <93397389+Dcastro99@users.noreply.github.com>
2022-09-21 22:08:58 -0700

    created trail dummy data

--- backend/src/Models/collectionClass.js
+++
@@ -1,9 +0,0 @@
-class Collection {
-    constructor(model, app) {
-        this.model = model;
-    }
-    async getList(req, res) {
-        const thing = await this.model.findAll();
-        res.status(200).send(thing);
-    }
-}
