17ac772ff721b23c86c1212acf0c7210100b2f71
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-09-22 01:14:12 -0400

    worked on the footer with Tony and Von

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,11 +1,60 @@
 import React from 'react'
-import { FooterContainer, FooterButton } from './FooterElements';
+import { FooterLogo, FooterA, FooterContainer, FooterLink, FooterLinkContainer, FooterLogoContainer, SocialMediaContainer, CopyRight, SocialMedia, SocialA, SocialMediaTitle, SocialMediaIcons } from './FooterElements';
+import Logo from '../../assets/images/GoTrek-4.png';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
+
+
 
 function Footer() {
   return (
     <FooterContainer>
-        <h1>Footer</h1>
-        <FooterButton>submit</FooterButton>
+      <FooterLogoContainer>
+        <FooterLogo>
+          <img src={Logo} alt="Logo" />
+        </FooterLogo>
+      </FooterLogoContainer>
+      <FooterLinkContainer>
+        <FooterLink>
+          <FooterA href="google.com" target="_blank">Google</FooterA>
+          <FooterA href="google.com" target="_blank">Google</FooterA>
+          <FooterA href="google.com" target="_blank">Google</FooterA>
+          <FooterA href="google.com" target="_blank">Google</FooterA>
+        </FooterLink>
+        <FooterLink>
+          <FooterA href="google.com" target="_blank">Google</FooterA>
+          <FooterA href="google.com" target="_blank">Google</FooterA>
+          <FooterA href="google.com" target="_blank">Google</FooterA>
+          <FooterA href="google.com" target="_blank">Google</FooterA>
+        </FooterLink>
+        <FooterLink>
+          <FooterA href="google.com" target="_blank">Google</FooterA>
+          <FooterA href="google.com" target="_blank">Google</FooterA>
+          <FooterA href="google.com" target="_blank">Google</FooterA>
+          <FooterA href="google.com" target="_blank">Google</FooterA>
+        </FooterLink>
+        <FooterLink>
+          <FooterA href="google.com" target="_blank">Google</FooterA>
+          <FooterA href="google.com" target="_blank">Google</FooterA>
+          <FooterA href="google.com" target="_blank">Google</FooterA>
+          <FooterA href="google.com" target="_blank">Google</FooterA>
+        </FooterLink>
+      </FooterLinkContainer>
+      <SocialMediaContainer>
+        <CopyRight>
+          <h3>CopyRight 2022</h3>
+        </CopyRight>
+        <SocialMedia>
+          <SocialMediaTitle>
+            <h4>Social Media</h4>
+          </SocialMediaTitle>
+          <SocialMediaIcons>
+            <SocialA href="instagram.com" target="_blank"></SocialA>
+            <SocialA href="google.com" target="_blank"></SocialA>
+            <FaFacebook />
+            <FaInstagram />
+          </SocialMediaIcons>
+        </SocialMedia>
+      </SocialMediaContainer>
     </FooterContainer>
   )
 }
