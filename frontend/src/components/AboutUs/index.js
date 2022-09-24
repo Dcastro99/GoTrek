9b7e747ab208431c1d6406453596542218930768
Von <107226923+ArzuVon@users.noreply.github.com>
2022-09-24 13:58:21 -0400

    removed material UI and added bootstrap

--- frontend/src/components/AboutUs/index.js
+++ frontend/src/components/AboutUs/index.js
@@ -1,12 +1,78 @@
 import React from 'react'
 import { AboutContainer, AboutWrapper } from './AboutElements';
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
 
 function About() {
   return (
     <AboutContainer>
         <AboutWrapper>
-            <h1>About Page</h1>
-        </AboutWrapper>
+    {/* <Card
+      component="section"
+      variant="gradient"
+      bgColor="dark"
+      position="relative"
+      py={6}
+      px={{ xs: 2, lg: 0 }}
+      mx={-2}
+    > */}
+      <Container>
+        <Grid container>
+          <Grid item xs={12} md={8} sx={{ mb: 6 }}>
+            <MKTypography variant="h3" color="white">
+              The Dev Team
+            </MKTypography>
+            <MKTypography variant="body2" color="white" opacity={0.8}>
+              There&apos;s nothing I really wanted to do in life that I wasn&apos;t able to get good
+              at. That&apos;s my skill.
+            </MKTypography>
+          </Grid>
+        </Grid>
+        <Grid container spacing={3}>
+          <Grid item xs={12} lg={6}>
+            <Card mb={1}>
+                image={team1}
+                name="Name"
+                position={{ color: "info", label: "UI Designer" }}
+                description="Artist is a term applied to a person who engages in an activity deemed to be an art."
+              />
+            </Card>
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <Card mb={1}>
+              <HorizontalTeamCard
+                image={team2}
+                name="Name"
+                position={{ color: "info", label: "Boss" }}
+                description="Artist is a term applied to a person who engages in an activity deemed to be an art."
+              />
+            </Card>
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <MKBox mb={{ xs: 1, lg: 0 }}>
+              <Card
+                image={team3}
+                name="Name"
+                position={{ color: "info", label: "Athlete" }}
+                description="Artist is a term applied to a person who engages in an activity deemed to be an art."
+              />
+            </MKBox>
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <MKBox mb={{ xs: 1, lg: 0 }}>
+              <HorizontalTeamCard
+                image={team4}
+                name="Name"
+                position={{ color: "info", label: "JS Developer" }}
+                description="Artist is a term applied to a person who engages in an activity deemed to be an art."
+              />
+            </MKBox>
+          </Grid>
+        </Grid>
+      </Container>
+    {/* </Card> */}
+</AboutWrapper>
     </AboutContainer>
   )
 }
