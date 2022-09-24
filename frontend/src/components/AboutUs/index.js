4dcc02ca3260b28e31b3886de42ed58e77704549
Von <107226923+ArzuVon@users.noreply.github.com>
2022-09-24 19:38:12 -0400

    About Us page w/devs and cards. Need Bios and LinkedIns

--- frontend/src/components/AboutUs/index.js
+++ frontend/src/components/AboutUs/index.js
@@ -1,46 +1,69 @@
 import React from 'react'
-import { AboutContainer, AboutWrapper } from './AboutElements';
+import { AboutContainer, AboutWrapper, AboutDev, AboutGoTrek, SocialMedia } from './AboutElements';
+import Card from 'react-bootstrap/Card';
+import {FaGithub, FaLinkedin} from 'react-icons/fa'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 // https://mdbootstrap.com/docs/react/getting-started/installation/
+import '../../assets/Styles/AboutUs.css'
 
 
+import CardGroup from 'react-bootstrap/CardGroup';
 import {
+  MDBContainer,
   MDBCard,
   MDBCardImage,
   MDBCardBody,
   MDBCardTitle,
   MDBCardLink,
   MDBListGroup,
-  MDBListGroupItem
+  MDBListGroupItem, 
 } from 'mdb-react-ui-kit';
-
-
-import CardGroup from 'react-bootstrap/CardGroup';
-
-
-
 
 function About() {
   return (
     <AboutContainer>
-  
-      <h1>About Us</h1>
-        <AboutWrapper>
-            
+      <AboutGoTrek>
+        <h1>About GoTrek</h1>
+      <MDBContainer>
+      <Card>
+      <Card.Body>
+      <h3><b>Our Purpose</b></h3>
+      <br></br>
+        <p>Spontaneous hikes can be overwhelming. If you are looking for a way to “Pack up quick and go” or “ready to go” 
+          kind of adventure. GoTrek provides an easy solution to hiking and exploration. If you’re an active hiker or a 
+          novice, GoTrek will provide you with all the information needed: prep, physicality level, expectations, 
+          weather/environment, supplies, and recommendations from others who have a good experience on said journey.
+           Whether it’s a quick day hike that’s 20 minutes away, or a week long backpacking trip deep into the mountains,
+          we have your back.</p>
+      <br></br>
+      <h3><b>Mission Statement</b></h3>
+      <p> Discover and stay connected with hiking trails happening near you!</p>
+
+     
+        </Card.Body>
+      </Card>
+    
+      </MDBContainer>
+      </AboutGoTrek>
+
+    <AboutDev>
+    <h1>Our Team</h1>
+    <AboutWrapper>
     <CardGroup>
-    <MDBCard>
+    <MDBCard className='card'>
       <MDBCardImage position='top' alt='...' src='https://avatars.githubusercontent.com/u/70992980?v=4' />
       <MDBCardBody>
         <MDBCardTitle>Tony Regalado</MDBCardTitle>
         <MDBCardBody>
-        Description      
+        Software Developer            
         </MDBCardBody>
       </MDBCardBody>
       <MDBListGroup flush>
-        <MDBListGroupItem> <MDBCardLink href='https://github.com/Edward-Regalado'>GitHub</MDBCardLink></MDBListGroupItem>
-        <MDBListGroupItem><MDBCardLink href='#'>LinkedIn</MDBCardLink></MDBListGroupItem>
-        <MDBListGroupItem><MDBCardLink href='#'>Twitter</MDBCardLink></MDBListGroupItem>
+        <MDBListGroupItem> 
+        <MDBCardLink href='https://github.com/Edward-Regalado'><SocialMedia><FaGithub/></SocialMedia></MDBCardLink></MDBListGroupItem>
+        <MDBListGroupItem><MDBCardLink href='https://www.linkedin.com/in/edward-regalado/'><SocialMedia><FaLinkedin/></SocialMedia></MDBCardLink></MDBListGroupItem>
       </MDBListGroup>
     </MDBCard>
     <MDBCard>
@@ -48,13 +71,12 @@ function About() {
       <MDBCardBody>
         <MDBCardTitle>Von Arzu</MDBCardTitle>
         <MDBCardBody>
-        Description      
+        Software Developer            
         </MDBCardBody>
       </MDBCardBody>
       <MDBListGroup flush>
-        <MDBListGroupItem> <MDBCardLink href='https://github.com/ArzuVon'>GitHub</MDBCardLink></MDBListGroupItem>
-        <MDBListGroupItem><MDBCardLink href='#'>LinkedIn</MDBCardLink></MDBListGroupItem>
-        <MDBListGroupItem><MDBCardLink href='#'>Twitter</MDBCardLink></MDBListGroupItem>
+        <MDBListGroupItem> <MDBCardLink href='https://github.com/ArzuVon'><SocialMedia><FaGithub/></SocialMedia></MDBCardLink></MDBListGroupItem>
+        <MDBListGroupItem><MDBCardLink href='https://www.linkedin.com/in/vonarzu'><SocialMedia><FaLinkedin/></SocialMedia></MDBCardLink></MDBListGroupItem>
       </MDBListGroup>
     </MDBCard>
     <MDBCard>
@@ -62,13 +84,12 @@ function About() {
       <MDBCardBody>
         <MDBCardTitle>Danny Castro</MDBCardTitle>
         <MDBCardBody>
-        Description      
+        Software Developer            
         </MDBCardBody>
       </MDBCardBody>
       <MDBListGroup flush>
-        <MDBListGroupItem> <MDBCardLink href='https://github.com/Dcastro99'>GitHub</MDBCardLink></MDBListGroupItem>
-        <MDBListGroupItem><MDBCardLink href='#'>LinkedIn</MDBCardLink></MDBListGroupItem>
-        <MDBListGroupItem><MDBCardLink href='#'>Twitter</MDBCardLink></MDBListGroupItem>
+        <MDBListGroupItem> <MDBCardLink href='https://github.com/Dcastro99'><SocialMedia><FaGithub/></SocialMedia></MDBCardLink></MDBListGroupItem>
+        <MDBListGroupItem><MDBCardLink href='#'><SocialMedia><FaLinkedin/></SocialMedia></MDBCardLink></MDBListGroupItem>
       </MDBListGroup>
     </MDBCard>
       <MDBCard>
@@ -76,13 +97,12 @@ function About() {
       <MDBCardBody>
         <MDBCardTitle>Sarah Teklemariam</MDBCardTitle>
         <MDBCardBody>
-        Description      
+        Software Developer            
         </MDBCardBody>
       </MDBCardBody>
       <MDBListGroup flush>
-        <MDBListGroupItem> <MDBCardLink href='https://github.com/SarahTek'>GitHub</MDBCardLink></MDBListGroupItem>
-        <MDBListGroupItem><MDBCardLink href='#'>LinkedIn</MDBCardLink></MDBListGroupItem>
-        <MDBListGroupItem><MDBCardLink href='#'>Twitter</MDBCardLink></MDBListGroupItem>
+        <MDBListGroupItem> <MDBCardLink href='https://github.com/SarahTek'><SocialMedia><FaGithub/></SocialMedia></MDBCardLink></MDBListGroupItem>
+        <MDBListGroupItem><MDBCardLink href='https://www.linkedin.com/in/sarah-t-31658b190/'><SocialMedia><FaLinkedin/></SocialMedia></MDBCardLink></MDBListGroupItem>
       </MDBListGroup>
     </MDBCard>
     <MDBCard>
@@ -90,18 +110,19 @@ function About() {
       <MDBCardBody>
         <MDBCardTitle>Justin Mathieu</MDBCardTitle>
         <MDBCardBody>
-        Description      
+        Software Developer            
         </MDBCardBody>
       </MDBCardBody>
       <MDBListGroup flush>
-        <MDBListGroupItem> <MDBCardLink href='https://github.com/Justin-Mathieu'>GitHub</MDBCardLink></MDBListGroupItem>
-        <MDBListGroupItem><MDBCardLink href='#'>LinkedIn</MDBCardLink></MDBListGroupItem>
-        <MDBListGroupItem><MDBCardLink href='#'>Twitter</MDBCardLink></MDBListGroupItem>
+        <MDBListGroupItem> <MDBCardLink href='https://github.com/Justin-Mathieu'><SocialMedia><FaGithub/></SocialMedia></MDBCardLink></MDBListGroupItem>
+        <MDBListGroupItem><MDBCardLink href='#'><SocialMedia><FaLinkedin/></SocialMedia></MDBCardLink></MDBListGroupItem>
       </MDBListGroup>
     </MDBCard>
     </CardGroup>
         </AboutWrapper>
+        </AboutDev>
     </AboutContainer>
+
   )
 }
 
