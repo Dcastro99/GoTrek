a9074bbfdd7e0ff5894c53f06c85388584536b25
Edward-Regalado <tregalado30@gmail.com>
2022-09-26 02:39:18 -0700

    added trails cards animation, started building map with react-map-gl

--- frontend/src/components/TrailCards/TrailCardsElements.js
+++ frontend/src/components/TrailCards/TrailCardsElements.js
@@ -2,52 +2,175 @@ import styled from 'styled-components';
 
 export const TrailsMainContainer = styled.div`
     width: 90%;
+    min-width: 400px;
     display: flex;
     align-items: center;
     justify-content: center;
     margin-bottom: 30px;
+    /* border: 5px solid red; */
 `;
 
 export const TrailCardsWrapper = styled.div`
     display: flex;
+    flex-wrap: wrap;
     flex-direction: column;
     align-items: center;
     justify-content: space-around;
-    width: 80%;
-    /* border: 2px solid black; */
-    /* height: 80%; */
-    /* background: pink; */
+    width: 100%;
+    min-width: 390px;
+    position: relative;
+
+    /* background: blue; */
+    /* border: 3px solid green; */
+
+    @media screen and (max-width: 768px) {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 90%;
+        /* height: 1000px; */
+    }
 `;
 
 export const TrailRowContainer = styled.div`
     display: flex;
+    flex-wrap: wrap;
     align-items: center;
-    justify-content: space-between;
+    justify-content: space-evenly;
+    position: relative;
+    height: 100%;
+    /* height: 400px; */
     width: 90%;
-    margin-top: 30px;
-    margin-bottom: 30px;
-    /* padding: 10px; */
-`;
-
-export const TrailCard = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    border-radius: 10px;
+    min-width: 380px;
+    /* margin-top: 30px; */
+    /* margin-bottom: 30px; */
+    /* background: pink; */
     /* border: 1px solid black; */
-    padding: 10px;
-    width: 25%;
-    margin: 20px;
-    box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.1);
 
-    &:hover {
-        transform: scale(1.08);
-        transition: all 0.3s ease-in-out;
+    @media screen and (max-width: 768px) {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+
+        /* background: lightgreen; */
+        /* width: 90%; */
     }
 `;
 
-export const TrailImage = styled.div`
+export const TrailCard = styled.div`
+    /* width: 25%; */
+    height: 20rem;
+    /* height: 100%; */
+    width: 20%;
+    /* min-width: 300px; */
+    min-width: 300px;
+    min-height: 300px;
+    perspective: 150rem;
+    position: relative;
+    /* position: absolute; */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 1rem;
+    box-shadow: none;
+    background: none;
+    cursor: pointer;
+    /* border: 1px solid black; */
+    
+    /* @media screen and (max-width: 1310px){
+        width: 40%;
+        min-width: 200px;
+        min-height: 300px;
+    }
+    @media screen and (max-width: 879px){
+        width: 40%;
+        min-width: 200px;
+    } */
+    
+    @media screen and (max-width: 768px){
+        width: 70%;
+        min-width: 250px;
+    }
+`;
+
+export const CardFront = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    width: 100%;
+    height: 100%;
+    /* height: 300px; */
+    min-width: 200px;
+    min-height: 250px;
+    
+    transition: all 0.8s ease;
+    backface-visibility: hidden;
+    position: relative;
+    top: 0;
+    left: 0;
+    /* padding:2rem; */
+
+    color: black;
+    border-radius: 15px;
+    box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.3);
+    /* border: 3px solid red; */
+    
+    &:hover {
+        transform: rotateY(180deg);
+    }
+`;
+
+export const CardBack = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    width: 100%;
+    height: 100%;
+    min-width: 250px;
+    min-height: 250px;
+    /* height: 25rem; */
+    
+    transition: all 0.8s ease;
+    backface-visibility: hidden;
+    position: absolute;
+    top: 0;
+    left: 0;
+    
+    /* padding:2rem; */
+    color: white;
+    
+    border-radius: 15px;
+    box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.3);
+    /* border: 3px solid red; */
+    
+    transform: rotate(-180deg);
+    
+    &:hover {
+        transform: rotateY(0deg);
+    }
+`;
+
+export const TrailImageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    width: 100;
+    margin: 10px;
+    /* min-width: 200px; */
+    height: 80%;
+    /* border: 1px solid black; */
+
+    h3 {
+        margin-top: 15px;
+        font-size: 1.5em;
+        /* color: #fff; */
+    }
+
     img{
         width: 100%;
         border-radius: 10px;
@@ -59,10 +182,20 @@ export const TrailInformation = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    /* background: lightblue; */
-    /* border: 2px solid black; */
+    text-align: center;
     width: 100%;
-    /* border: 2px solid black; */
-    margin-top: 10px;
+    height: 100%;
+    padding: 10px;
+    /* color: white; */
+    /* margin-top: 10px; */
+    /* background: lightblue; */
+    text-align: center;
+    /* border: 1px solid black; */
+    h5 {
+        font-size: 1.1rem;
+    }
+    P {
+        font-size: 1rem;
+    }
 `;
 
--- frontend/src/components/TrailCards/index.js
+++ frontend/src/components/TrailCards/index.js
@@ -1,95 +1,32 @@
 import React from 'react'
-import { TrailsMainContainer, TrailRowContainer, TrailCardsWrapper, TrailCard, TrailImage, TrailInformation } from './TrailCardsElements';
+import { TrailsMainContainer, TrailRowContainer, TrailCardsWrapper, TrailCard, CardFront, CardBack, TrailImageContainer, TrailInformation } from './TrailCardsElements';
+import trailData from '../../assets/data/test-data.json';
 import photo from '../../assets/images/hike.jpg';
+import '../../assets/styles/cards.css'
 
 function TrailCards() {
+
   return (
     <TrailsMainContainer>
         <TrailCardsWrapper>
             <TrailRowContainer>
-                <TrailCard>
-                    <TrailImage>
-                        <img src={photo} alt='hiking' />
-                    </TrailImage>
-                    <TrailInformation>
-                        <h3>Trail Name</h3>
-                        <h5>Distance: 10 miles</h5>
-                        <p>Information</p>
-                    </TrailInformation>
-                </TrailCard>
-                <TrailCard>
-                    <TrailImage>
-                        <img src={photo} alt='hiking' />
-                    </TrailImage>
-                    <TrailInformation>
-                        <h3>Trail Name</h3>
-                        <h5>Distance: 10 miles</h5>
-                        <p>Information</p>
-                    </TrailInformation>
-                </TrailCard>
-                <TrailCard>
-                    <TrailImage>
-                        <img src={photo} alt='hiking' />
-                    </TrailImage>
-                    <TrailInformation>
-                        <h3>Title Name</h3>
-                        <h5>Distance: 10 miles</h5>
-                        <p>Information</p>
-                    </TrailInformation>
-                </TrailCard>
-                <TrailCard>
-                    <TrailImage>
-                        <img src={photo} alt='hiking' />
-                    </TrailImage>
-                    <TrailInformation>
-                        <h3>Title Name</h3>
-                        <h5>Distance: 10 miles</h5>
-                        <p>Information</p>
-                    </TrailInformation>
-                </TrailCard>
-                
-            </TrailRowContainer>
-            <TrailRowContainer>
-                <TrailCard>
-                    <TrailImage>
-                        <img src={photo} alt='hiking' />
-                    </TrailImage>
-                    <TrailInformation>
-                        <h3>Title Name</h3>
-                        <h5>Distance: 10 miles</h5>
-                        <p>Information</p>
-                    </TrailInformation>
-                </TrailCard>
-                <TrailCard>
-                    <TrailImage>
-                        <img src={photo} alt='hiking' />
-                    </TrailImage>
-                    <TrailInformation>
-                        <h3>Title Name</h3>
-                        <h5>Distance: 10 miles</h5>
-                        <p>Information</p>
-                    </TrailInformation>
-                </TrailCard>
-                <TrailCard>
-                    <TrailImage>
-                        <img src={photo} alt='hiking' />
-                    </TrailImage>
-                    <TrailInformation>
-                        <h3>Title Name</h3>
-                        <h5>Distance: 10 miles</h5>
-                        <p>Information</p>
-                    </TrailInformation>
-                </TrailCard>
-                <TrailCard>
-                    <TrailImage>
-                        <img src={photo} alt='hiking' />
-                    </TrailImage>
-                    <TrailInformation>
-                        <h3>Title Name</h3>
-                        <h5>Distance: 10 miles</h5>
-                        <p>Information</p>
-                    </TrailInformation>
-                </TrailCard>
+                {trailData.map(trail => (
+                <TrailCard className='card'>
+                        <CardFront className='card-side front'>
+                            <TrailImageContainer>
+                                <img src={photo} alt='hiking' />
+                                <h3>{trail.name}</h3>
+                            </TrailImageContainer>
+                        </CardFront>
+                        <CardBack className='card-side back'>
+                            <TrailInformation>
+                                <h5>Length: {trailData.length}</h5>
+                                <h5>Elevation: {trail.elevation.Gain}</h5>
+                                <p>Features: {trail.features}</p>
+                            </TrailInformation>
+                        </CardBack> 
+                    </TrailCard>
+                ))};
             </TrailRowContainer>
         </TrailCardsWrapper>
     </TrailsMainContainer>
