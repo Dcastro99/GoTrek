6a2444ed14d2b85f30f3470caf9e3295e5684c70
Von <107226923+ArzuVon@users.noreply.github.com>
2022-09-26 15:57:17 -0400

    Cannot find bug and reformatted

--- frontend/src/components/Explore/index.js
+++ frontend/src/components/Explore/index.js
@@ -1,19 +1,18 @@
-import React from 'react'
-import { ExploreContainer, ExploreWrapper } from './ExploreElements';
-import { ChakraProvider, theme } from '@chakra-ui/react'
-import Map from '../GMap'
-
+import React from "react";
+import { ExploreContainer, ExploreWrapper } from "./ExploreElements";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import GMap from "../GMap";
 
 function Explore() {
   return (
     <ExploreContainer>
-        <ExploreWrapper>
+      <ExploreWrapper>
         <ChakraProvider theme={theme}>
-            <Map />
-        </ChakraProvider>  
+          <GMap />
+        </ChakraProvider>
       </ExploreWrapper>
     </ExploreContainer>
-  )
+  );
 }
 
 export default Explore;
