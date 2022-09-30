118978ec18b782f3a5f00561fe39008b0590fe5e
Edward-Regalado <tregalado30@gmail.com>
2022-09-30 00:50:05 +0000

    login/logout buttons

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -1,13 +1,13 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
-import { Button } from 'react-bootstrap';
+// import { Button } from 'react-bootstrap';
 import '../../assets/styles/Login.css'
 
 const LoginButton = () => {
   const { isAuthenticated, loginWithRedirect } = useAuth0();
 
   if (!isAuthenticated)
-    return <Button id="loginBtn" variant="contained" onClick={() => loginWithRedirect()}> Log In</Button >;
+    return <button class="loginBtn" variant="contained" onClick={() => loginWithRedirect()}> Log In</button>;
 };
 
 export default LoginButton;
--- frontend/src/components/Logout/index.js
+++ frontend/src/components/Logout/index.js
@@ -1,6 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
-import { Button } from 'react-bootstrap';
+// import { Button } from 'react-bootstrap';
 import '../../assets/styles/Logout.css'
 
 const LogoutButton = () => {
@@ -8,9 +8,9 @@ const LogoutButton = () => {
 
   return (
 
-    <Button id="logOut" variant="outlined" onClick={() => logout({ returnTo: window.location.origin })}>
+    <button class="logOut" variant="outlined" onClick={() => logout({ returnTo: window.location.origin })}>
       Log Out
-    </Button>
+    </button>
 
   );
 };
