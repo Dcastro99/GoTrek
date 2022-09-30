b113bd916a3468a53268e8a51387071e4df614f0
Edward-Regalado <tregalado30@gmail.com>
2022-09-30 00:35:45 +0000

    fixed weather icons

--- frontend/src/components/Weather/src/Components/Forecast/forecast.js
+++ frontend/src/components/Weather/src/Components/Forecast/forecast.js
@@ -17,7 +17,7 @@ const Forecast = ({ data }) => {
             <div>
               <div>
                 <div className="daily-item">
-                  <img src={`icons/${item.weather[0].icon}.png`} className="icon-small" alt="weather" />
+                  <img src={`/icons/${item.weather[0].icon}.png`} className="icon-small" alt="weather" />
                   <label className="day">{forecastDays[idx]}</label>
                   {/* <label className="description">{item.weather[0].description}</label> */}
                   {/* <label className="min-max">{Math.round(item.main.temp_max)}°F /{Math.round(item.main.temp_min)}°F</label> */}
