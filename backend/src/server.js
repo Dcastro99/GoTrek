75e75797aad411d044a19126175ddc9aec925769
Danny <93397389+Dcastro99@users.noreply.github.com>
2023-01-19 17:17:40 -0800

    env fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -48,6 +48,7 @@ io.on('connection', socket => {
 
 // MongoDB
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
